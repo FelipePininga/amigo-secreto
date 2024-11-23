ad0de52c22d70ef0b2b9baac152e89c8485b7d6f
FelipePininga <felipepininga@gmail.com>
2024-11-23 09:18:49 -0300

    Implementando botão reiniciar

--- js/app.js
+++ js/app.js
@@ -29,3 +29,8 @@ function embaralha(lista){
         [lista[indice-1],lista[indiceAleatorio]]=[lista[indiceAleatorio],lista[indice-1]];
     }
 }
+function reiniciar(){
+    amigos=[];
+    document.getElementById('lista-amigos').innerHTML=''; 
+    document.getElementById('lista-sorteio').innerHTML='';  
+}
